8730606b659118b6707990acc9d95ce1313ffe2b
BerylAthaya <130304629+BerylAthaya@users.noreply.github.com>
2023-12-15 07:45:38 +0700

    Update finalproject3.py

--- finalproject3.py
+++ finalproject3.py
@@ -8,7 +8,7 @@ conn = st.connection("postgresql", type="sql",
                      url="postgresql://avindalutfiapb17:VNvKwPZaJ32O@ep-snowy-heart-34856175.us-east-2.aws.neon.tech/web?sslmode=require")
 with conn.session as session:
     query = text('CREATE TABLE IF NOT EXISTS SCHEDULE (id serial, bank_name varchar, nasabah_name varchar, gender char(25), \
-                                                       jenis_tabungan varchar, rekening varchar, alamat text, tanggal_pembuatan date);')
+                                                       jenis_tabungan varchar, saldo varchar, alamat text, tanggal_pembuatan date);')
     session.execute(query)
 
 st.header('DATABASE NASABAH BANK')
@@ -21,7 +21,7 @@ if page == "View Data":
 if page == "Edit Data":
     if st.button('Tambah Data'):
         with conn.session as session:
-            query = text('INSERT INTO schedule (bank_name, nasabah_name, gender, jenis_tabungan, rekening, alamat, waktu_pembuatan, tanggal_pembuatan) \
+            query = text('INSERT INTO schedule (bank_name, nasabah_name, gender, jenis_tabungan, saldo, alamat, waktu_pembuatan, tanggal_pembuatan) \
                           VALUES (:1, :2, :3, :4, :5, :6, :7, :8);')
             session.execute(query, {'1':'', '2':'', '3':'', '4':'[]', '5':'', '6':'', '7':None, '8':None})
             session.commit()
@@ -33,7 +33,7 @@ if page == "Edit Data":
         nasabah_name_lama = result["nasabah_name"]
         gender_lama = result["gender"]
         jenis_tabungan_lama = result["jenis_tabungan"]
-        rekening_lama = result["rekening"]
+        saldo_lama = result["saldo"]
         alamat_lama = result["alamat"]
         waktu_pembuatan_lama = result["waktu_pembuatan"]
         tanggal_pembuatan_lama = result["tanggal_pembuatan"]
@@ -44,7 +44,7 @@ if page == "Edit Data":
                 nasabah_name_baru = st.text_input("nasabah_name", nasabah_name_lama)
                 gender_baru = st.selectbox("gender", list_gender, list_gender.index(gender_lama))
                 jenis_tabungan_baru = st.text_input("jenis_tabungan", jenis_tabungan_lama)
-                rekening_baru = st.text_input("rekening", rekening_lama)
+                saldo_baru = st.text_input("saldo", saldo_lama)
                 alamat_baru = st.text_input("alamat", alamat_lama)
                 waktu_pembuatan_baru = st.time_input("waktu_pembuatan", waktu_pembuatan_lama)
                 tanggal_pembuatan_baru = st.date_input("tanggal_pembuatan", tanggal_pembuatan_lama)
@@ -56,10 +56,10 @@ if page == "Edit Data":
                         with conn.session as session:
                             query = text('UPDATE schedule \
                                           SET bank_name=:1, nasabah_name=:2, gender=:3, jenis_tabungan=:4, \
-                                          rekening=:5, alamat=:6, waktu_pembuatan=:7, tanggal_pembuatan=:8 \
+                                          saldo=:5, alamat=:6, waktu_pembuatan=:7, tanggal_pembuatan=:8 \
                                           WHERE id=:9;')
                             session.execute(query, {'1':bank_name_baru, '2':nasabah_name_baru, '3':gender_baru, '4':str(jenis_tabungan_baru), 
-                                                    '5':rekening_baru, '6':alamat_baru, '7':waktu_pembuatan_baru, '8':tanggal_pembuatan_baru, '9':id})
+                                                    '5':saldo_baru, '6':alamat_baru, '7':waktu_pembuatan_baru, '8':tanggal_pembuatan_baru, '9':id})
                             session.commit()
                             st.experimental_rerun()
                 
