41a9e985636ac091d8de33fb2428598a70228006
BerylAthaya <130304629+BerylAthaya@users.noreply.github.com>
2023-12-15 07:32:32 +0700

    Update finalproject3.sql

--- finalproject3.sql
+++ finalproject3.sql
@@ -5,7 +5,7 @@ create table schedule (
 	nasabah_name text,
 	gender text,
 	jenis_tabungan text,
-	rekening string,
+	rekening str,
    	alamat text,
  	waktu_pembuatan time,
 	tanggal_pembuatan date,
